4f65575d8753e454f9ae9007787c226f68b30d2d
QWEL.DESIGN <webmaster@qwel.design>
2026-02-26 13:54:03 +0900

    feat: create Icon component

---
+++ src/primitives/Icon.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  name: string,
+  icon: string,
+  color:string,
+  size: string,
+  rounded: boolean
+}>()
+</script>
+
+<template>
+  <button :class="name">
+    <div :class="`icon is-${icon} is-${color} is-${size} ${rounded ? 'is-rounded' : ''}`">
+      <span class="icon__span"></span>
+    </div>
+  </button>
+</template>
